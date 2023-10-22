ed6d75fff77ef5315f607e77ddc339371b71fd81
alinabodnar1 <malina.bodnar@gmail.com>
2023-10-22 22:30:03 +0300

    add rest tasks

--- 01_cli_interactive_sort/app.js
+++ 01_cli_interactive_sort/app.js
@@ -8,41 +8,59 @@ let questions = [
   "3 - Show numbers from bigger to smaller",
   "4 - Display words in ascending order by number of letters in the word",
   "5 - Show only unique words",
-  "6 - Display only unique values from the set of words and numbers entered by the user"
-]
+  "6 - Display only unique values from the set of words and numbers entered by the user",
+];
 
 let answers = [];
 
-process.stdin.on("data", function(data){
+process.stdin.on("data", function (data) {
   answers.push(data.toString().trim());
 
-console.log("You entered: ", answers); 
- console.log(`How to sort input data?`);
+  console.log("You entered: ", answers);
+  console.log(`How to sort input data?`);
 
-  questions.forEach(question => console.log(question));
+  questions.forEach((question) => console.log(question));
   process.stdout.write("Select 1 - 6 and press ENTER  >  ");
 
-  const items = answers[0].split(' '); // perforn array into items
-  arraySymbols = items.filter(element => element.trim() !== ''); // remove empty lines
+  const items = answers[0].split(" "); // perforn array into items
+  arrayData = items.filter((element) => element.trim() !== ""); // remove empty lines
 
   // 1 - Sort words alphabetically (A-Z)
   if (answers.includes("1")) {
-    console.log('Sorted answers:', arraySymbols.sort()); 
+    console.log(arrayData.sort());
   }
 
   // 2 - Show numbers from lesser to greater
   if (answers.includes("2")) {
-
-    console.log('Sorted answers:', arraySymbols.sort((a, b) => a - b)); 
+    console.log(arrayData.sort((a, b) => a - b));
+  }
+  // 3 - Show numbers from bigger to smaller
+  if (answers.includes("3")) {
+    console.log(arrayData.sort((a, b) => b - a));
+  }
+  // 4 - Display words in ascending order by number of letters in the word
+  if (answers.includes("4")) {
+    console.log(arrayData.sort((a, b) => a.length - b.length));
   }
 
-  if (answers.includes("2")) {
-    console.log('Sorted answers:', arraySymbols.sort((a, b) => a - b)); 
+  // 5 - Show only unique words
+  if (answers.includes("5")) {
+    let uniqueWords = arrayData.filter(
+      (word, index, array) => array.indexOf(word) === index
+    );
+    console.log(uniqueWords);
   }
+
+  // 6 - Display only unique values from the set of words and numbers entered by the user
+  if (answers.includes("6")) {
+    let uniqueWords = arrayData.filter(
+      (word, index, array) => array.indexOf(word) === index
+    );
+    console.log(uniqueWords);
+  }
+
   // exit the program
   if (answers.includes("exit")) {
     process.exit();
   }
-
 });
-
